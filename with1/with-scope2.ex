c3c7021b934fbe112c6da4bf451212f71096d723
leogtzr <leogutierrezramirez@gmail.com>
2021-02-06 17:14:38 -0700

    formatting correctly script to parse etc/passwd"

--- with1/with-scope2.ex
+++ with1/with-scope2.ex
@@ -1,13 +1,13 @@
 content = "Now is the time"
 
 # Reads /etc/passwd with local scope using with...
-lp = with {:ok, file} = File.open("/etc/passwd"),
-  content = IO.read(file, :all),
-  :ok = File.close(file),
-  [_, uid, gid] = Regex.run(~r/^leo:.*?:(\d+):(\d+)/m, content)
-  do
-      "Group; #{gid}, UID: #{uid}"
+lp =
+  with {:ok, file} = File.open("/etc/passwd"),
+       content = IO.read(file, :all),
+       :ok = File.close(file),
+       [_, uid, gid] = Regex.run(~r/^leo:.*?:(\d+):(\d+)/m, content) do
+    "Group; #{gid}, UID: #{uid}"
   end
 
-IO.puts lp
-IO.puts content
+IO.puts(lp)
+IO.puts(content)
