680668ff23787b31d7184f7137428917ff0d0d91
leogtzr <leogutierrezramirez@gmail.com>
2021-03-04 19:41:51 -0700

    add nested struct example

---
+++ struct1/defstruct2.exs
@@ -0,0 +1,9 @@
+defmodule Attendee do
+
+  defstruct name: "", paid: false, over_18: true
+
+  def may_attend_after_party(attendee = %Attendee{}) do
+    attendee.paid && attendee.over_18
+  end
+
+end
