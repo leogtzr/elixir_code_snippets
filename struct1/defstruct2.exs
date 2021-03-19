4c2821adf62fd34206e97a99ca816c1ff5f33c4f
leogtzr <leogutierrezramirez@gmail.com>
2021-03-19 16:44:51 -0700

    add running structure ...

--- struct1/defstruct2.exs
+++ struct1/defstruct2.exs
@@ -5,5 +5,13 @@ defmodule Attendee do
   def may_attend_after_party(attendee = %Attendee{}) do
     attendee.paid && attendee.over_18
   end
+  
+  def print_vip_badge(%Attendee{name: name}) when name != "" do
+    IO.puts "Very cheap badge for #{name}"
+  end
+  
+  def print_vip_badge(%Attendee{}) do
+    raise "missing name for badge"
+  end
 
 end
